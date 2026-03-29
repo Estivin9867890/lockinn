2069a69688068c54084c56cf0bc54d92245d30b2
Louis ESTIVIN <louisestivin@MacBook-Air-de-Louis.local>
2026-03-29 17:00:22 +0200

    Fix useSearchParams Suspense boundary

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import MainLayout from "@/components/layout/MainLayout";
 import SettingsPage from "@/components/settings/SettingsPage";
 
 export default function Settings() {
   return (
     <MainLayout>
-      <SettingsPage />
+      <Suspense>
+        <SettingsPage />
+      </Suspense>
     </MainLayout>
   );
 }
